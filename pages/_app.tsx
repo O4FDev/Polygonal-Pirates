f5a57fcde7070747d582f7530dd8fdfad1cff5fb
O4FDev <luke.lucas@ou.ac.uk>
2022-07-14 21:29:22 +0100

    feat: auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+
+const publicPages = [] as string[];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+
+  // If the current route is listed as public, render it directly
+  // Otherwise, use Clerk to require authentication
+  return (
+    <ClerkProvider>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 }
 
 export default MyApp
